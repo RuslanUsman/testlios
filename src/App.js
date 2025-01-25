aaf82e65ae10d94e7d4cfc788219b7755c69f302
Руслан Усманов <ruslanusmanov029@gmail.com>
2025-01-25 11:46:40 +0300

    Deploy to GitHub

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import MainMenu from './components/MainMenu';
 import MaterialSelection from './components/MaterialSelection';
 import ConstructionSelection from './components/ConstructionSelection';
